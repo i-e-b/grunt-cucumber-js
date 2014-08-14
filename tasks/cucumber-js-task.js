4d560f2f1273de5db463fe33e651f2fbc8861e27
Andrew Benjamin <andy_J_B@hotmail.co.uk>
2014-08-14 10:02:12 +0100

    Fixing a merge error in the cucumber task runner

--- tasks/cucumber-js-task.js
+++ tasks/cucumber-js-task.js
@@ -10,107 +10,21 @@ module.exports = function (grunt) {
 
         var steps = options.steps;
         var tags = options.tags;
-        var stackFormat = options.stackFormat
+        var useShortStackTraces = options.useShortStackTraces;
         var format = options.format;
         var modulePath = options.modulePath;
         var coffee = options.coffee;
 
         grunt.verbose.writeflags(options, 'Options');
 
-        var callback = function (succeeded) {
-            var exitFunction = function () {
+        var callback = function(succeeded) {
+            var exitFunction = function() {
                 done(succeeded);
             };
 
             // --- exit after waiting for all pending output ---
             var waitingIO = false;
-            module.exports = function (grunt) {
-
-                // The Cucumber Task
-                grunt.registerMultiTask('cucumberjs', 'Runs cucumber.js', function () {
-                    // Make this task async
-                    var done = this.async();
-
-                    // Load all the options
-                    var options = this.options();
-
-                    var steps = options.steps;
-                    var tags = options.tags;
-                    var format = options.format;
-                    var useShortStackTraces = options.useShortStackTraces;
-                    var modulePath = options.modulePath;
-                    var coffee = options.coffee;
-
-                    grunt.verbose.writeflags(options, 'Options');
-
-                    var callback = function (succeeded) {
-                        var exitFunction = function () {
-                            done(succeeded);
-                        };
-
-                        // --- exit after waiting for all pending output ---
-                        var waitingIO = false;
-                        process.stdout.on('drain', function () {
-                            if (waitingIO) {
-                                // the kernel buffer is now empty
-                                exitFunction();
-                            }
-                        });
-                        if (process.stdout.write("")) {
-                            // no buffer left, exit now:
-                            exitFunction();
-                        } else {
-                            // write() returned false, kernel buffer is not empty yet...
-                            waitingIO = true;
-                        }
-                    };
-
-                    var files = this.filesSrc;
-
-
-                    var execOptions = ['node', 'node_modules/.bin/cucumber-js'];
-
-                    var _ = grunt.util._;
-                    if (!_.isEmpty(files)) {
-                        execOptions = execOptions.concat(files);
-                    }
-
-                    if (!_.isEmpty(steps)) {
-                        execOptions.push('-r');
-                        execOptions.push(steps);
-                    }
-
-                    if (!_.isEmpty(tags)) {
-                        execOptions.push('-t');
-                        execOptions.push(tags);
-                    }
-
-                    if (useShortStackTraces) {
-                        execOptions.push('--shortStackTraces');
-                    }
-
-                    if (!_.isEmpty(format)) {
-                        execOptions.push('-f');
-                        execOptions.push(format);
-                    }
-
-                    if (coffee) {
-                        execOptions.push('--coffee');
-                    }
-
-                    var cucumberPath = 'cucumber';
-                    if (!_.isEmpty(modulePath)) {
-                        cucumberPath = modulePath;
-                    }
-
-                    grunt.verbose.writeln('Exec Options: ' + execOptions.join(' '));
-                    var cucumber = require(cucumberPath);
-                    cucumber.Cli(execOptions).run(callback);
-
-                });
-            };
-
-            process.stdout.on('drain', function () {
+            process.stdout.on('drain', function() {
                 if (waitingIO) {
                     // the kernel buffer is now empty
                     exitFunction();
@@ -131,26 +45,25 @@ module.exports = function (grunt) {
         var execOptions = ['node', 'node_modules/.bin/cucumber-js'];
 
         var _ = grunt.util._;
-        if (!_.isEmpty(files)) {
+        if (! _.isEmpty(files)) {
             execOptions = execOptions.concat(files);
         }
 
-        if (!_.isEmpty(stackFormat)) {
-            execOptions.push('-s');
-            execOptions.push(stackFormat);
+        if (useShortStackTraces) {
+            execOptions.push('--shortStackTraces');
         }
 
-        if (!_.isEmpty(steps)) {
+        if (! _.isEmpty(steps)) {
             execOptions.push('-r');
             execOptions.push(steps);
         }
 
-        if (!_.isEmpty(tags)) {
+        if (! _.isEmpty(tags)) {
             execOptions.push('-t');
             execOptions.push(tags);
         }
 
-        if (!_.isEmpty(format)) {
+        if (! _.isEmpty(format)) {
             execOptions.push('-f');
             execOptions.push(format);
         }
@@ -160,7 +73,7 @@ module.exports = function (grunt) {
         }
 
         var cucumberPath = 'cucumber';
-        if (!_.isEmpty(modulePath)) {
+        if (! _.isEmpty(modulePath)) {
             cucumberPath = modulePath;
         }
 
